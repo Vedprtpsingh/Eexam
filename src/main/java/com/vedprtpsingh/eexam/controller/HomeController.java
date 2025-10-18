274f451fcb795dfebe1eac59e95205a4c0663558
Ved Pratap Singh <singhvedpratap14@gmail.com>
2025-10-18 23:56:04 +0530

    Kuch Axa Kro

--- src/main/java/com/vedprtpsingh/eexam/controller/HomeController.java
+++ src/main/java/com/vedprtpsingh/eexam/controller/HomeController.java
@@ -1,6 +1,4 @@
 package com.vedprtpsingh.eexam.controller;
-
-import org.hibernate.annotations.TimeZoneStorage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,11 +7,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Home";
-    }
-
-    @GetMapping("/flana")
-    public String hrm() {
         return "Home";
     }
 }
